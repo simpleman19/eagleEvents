f3071a0fae9a0b639dba1f087e1f3df3984d8a67
jacob <jake6642@gmail.com>
2018-07-25 21:22:01 -0500

    UI selection mostly working

--- eagleEvents/routes/events.py
+++ eagleEvents/routes/events.py
@@ -1,12 +1,60 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, Flask, flash, request, redirect, url_for
+from eagleEvents.models.event import Event
+from eagleEvents.models.company import Company
+import os
+from werkzeug.utils import secure_filename
+UPLOAD_FOLDER = '/tmp'
+ALLOWED_EXTENSIONS = set(['csv'])
 events_blueprint = Blueprint('events', __name__)
 
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@events_blueprint.route('/listEvents')
-def list_events():
+
+@events_blueprint.route('/listEvents/<filename>')
+def list_events(filename):
     # TODO LIST
-    return render_template('test.html.j2')
+    c = Company()
+    event = Event.query.filter_by(name='Test Event').first()
+    path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    c.process_guest_list(path, event)
+    return render_template('test.html.j2', test=filename)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@events_blueprint.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    event = Event.query.filter_by(name='Test Event').first()
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(path)
+            print(path)
+            return redirect(url_for('events.list_events', filename=filename))
+
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file onchange="this.form.submit()" name=file>
+    </form>
+    '''
 
 
 @events_blueprint.route('/modifyEvent')
