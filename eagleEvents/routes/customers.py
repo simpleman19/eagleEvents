e17cdd404fde8e6ac90f817dbd3be91fb944e071
emilypie <empielemeier@gmail.com>
2018-07-31 14:50:58 -0500

    Flash validation messages on save

--- eagleEvents/routes/customers.py
+++ eagleEvents/routes/customers.py
@@ -20,12 +20,14 @@ def add_customer():
         return render_template('add-update-customer.html.j2', customer=customer,
                                cancel_redirect=url_for('customers.list_customers'))
     else:
-        customer.name = request.form['name']
-        customer.email = request.form['email']
-        customer.phone_number = request.form['phone']
-        db.session.add(customer)
-        db.session.commit()
-        return redirect(url_for('customers.list_customers'))
+        is_valid = validate_and_save(customer, request)
+        if is_valid:
+            return redirect(url_for('customers.list_customers'))
+        else:
+            return render_template('add-update-customer.html.j2', customer=customer,
+                                   cancel_redirect=url_for('customers.list_customers'))
+
+
 
 @customers_blueprint.route('/modifyCustomer/<customer_id>', methods=['GET', 'POST'])
 @multi_auth.login_required
@@ -35,10 +37,22 @@ def modify_customer(customer_id):
         return render_template('add-update-customer.html.j2', customer=customer,
                                cancel_redirect=url_for('customers.list_customers'))
     else:
-        customer.name = request.form['name']
-        customer.email = request.form['email']
-        customer.phone_number = request.form['phone']
+        is_valid = validate_and_save(customer, request)
+        if is_valid:
+            return redirect(url_for('customers.list_customers'))
+        else:
+            return render_template('add-update-customer.html.j2', customer=customer,
+                                   cancel_redirect=url_for('customers.list_customers'))
+
+
+def validate_and_save(customer, request):
+    customer.name = request.form['name']
+    customer.email = request.form['email']
+    customer.phone_number = request.form['phone']
+    if customer.name is None or len(customer.name) == 0:
+        flash("Name is required", "error")
+        return False
+    else:
         db.session.add(customer)
         db.session.commit()
-        return redirect(url_for('customers.list_customers'))
-
+        return True
