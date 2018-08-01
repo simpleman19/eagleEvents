d0a95a88ecda8365e2828aeea0c88457ce2b3d03
paresa <paresa@paresas-mbp.attlocal.net>
2018-08-01 02:12:39 -0500

    Made some changes based on the defect comments

--- eagleEvents/routes/event_planners.py
+++ eagleEvents/routes/event_planners.py
@@ -6,7 +6,7 @@ event_planners_blueprint = Blueprint('event_planners', __name__)
 @event_planners_blueprint.route('/listEventPlanners')
 def list_event_planners():
     # TODO List event planners
-    return render_template('test.html.j2')
+    return render_template('user.html.j2')
 
 
 @event_planners_blueprint.route('/modifyEventPlanner')
