1647615ff901f10802b1c734f576c8f690fbc75d
dee-dee-raye <deedee.mccroskey@gmail.com>
2018-08-03 17:38:40 -0500

    fixing some of my links

--- eagleEvents/routes/events.py
+++ eagleEvents/routes/events.py
@@ -13,11 +13,11 @@ events_blueprint = Blueprint('events', __name__)
 @events_blueprint.route('/listEvents')
 @multi_auth.login_required
 def list_events():
-    show_all = request.args['show_all']
+    show_all = request.args.get("show_all")
     currentUser = g.current_user
     company_id_user = currentUser.company_id
     events_of_company = Event.query.filter_by(company_id = company_id_user).order_by(Event.time.desc())
-    if show_all == 'yes':
+    if show_all is not None:
         events = events_of_company
     else:
         events = events_of_company.filter_by(planner_id = currentUser.id)
