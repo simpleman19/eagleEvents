37af42c14a9e431a878fedb1a254b3700e4c364f
jacob <jake6642@gmail.com>
2018-08-03 22:34:52 -0500

    retaining info and submitting successfully

--- eagleEvents/models/event.py
+++ eagleEvents/models/event.py
@@ -1,7 +1,7 @@
 from eagleEvents.seating_chart_ga import SeatingChartGA
 from . import db
 from eagleEvents.models import Guest
-import uuid
+import uuid, datetime
 from eagleEvents.models import Guest
 from sqlalchemy_utils import UUIDType
 from typing import List
@@ -10,11 +10,11 @@ from typing import List
 class Event(db.Model):
     __tablename__ = 'event'
     id = db.Column(UUIDType(binary=False), primary_key=True, default=uuid.uuid4)
-    time = db.Column(db.DateTime, nullable=False)
+    time = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now())
     is_done = db.Column(db.Boolean, nullable=False, default=False)
     venue = db.Column(db.String(150), nullable=False, default="")
     name = db.Column(db.String(150), nullable=False, default="")
-    percent_extra_seats = db.Column(db.Float, nullable=False)
+    percent_extra_seats = db.Column(db.Float, nullable=False, default=10)
     customer_id = db.Column(UUIDType(binary=False), db.ForeignKey('customer.id'))
     customer = db.relationship('Customer', lazy=False)
     company_id = db.Column(UUIDType(binary=False), db.ForeignKey('company.id'))
@@ -29,6 +29,7 @@ class Event(db.Model):
 
     def __init__(self, customer):
         self.customer = customer
+        self.company = customer.company
 
     def set_guests(self, guests: List['Event']):
         _guests = guests
--- eagleEvents/routes/events.py
+++ eagleEvents/routes/events.py
@@ -5,8 +5,9 @@ from flask import Blueprint, render_template, Flask, flash, request, redirect, u
 from eagleEvents.models.event import Event
 from eagleEvents.models.customer import Customer
 from eagleEvents.models.user import User
+from eagleEvents.models.company import TableSize
 from eagleEvents import db
-import os, config
+import os, config, datetime
 from werkzeug.utils import secure_filename
 from eagleEvents.auth import multi_auth
 ALLOWED_EXTENSIONS = set(['csv'])
@@ -21,73 +22,82 @@ def list_events():
     return render_template('event.html.j2')
 
 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in set(['csv'])
-
-
 @events_blueprint.route('/addEvent', methods=['GET', 'POST'])
 @multi_auth.login_required
 def add_event():
-    event = None
-    if request.method == 'GET':
-        planner_list = User.query.all()
-        customer_list = Customer.query.all()
-        return render_template('add-update-event.html.j2', customers=customer_list, planners=planner_list,
-                               cancel_redirect=url_for('events.list_events'))
-    elif request.method == 'POST':
-        if event is None:
-            customer = request.form['customer']
-            event = Event(customer)
-        if request.form['submit'] == 'save':
-            is_valid = validate_and_save(event, request)
-            if is_valid:
-                flash("{name} added".format(name=event.name), "success")
-                return redirect(url_for('events.list_events'))
-            else:
-                return render_template('add-update-customer.html.j2', event=event,
-                                       cancel_redirect=url_for('events.list_events'))
-        elif request.form['cancel'] == 'cancel':
-            redirect(url_for('events.list_events'))
-        else:
-            upload_file(request, event)
-    else:
-        return render_template('add-update-customer.html.j2', event=event,
-                               cancel_redirect=url_for('events.list_events'))
+    planner_list = User.query.all()
+    customer_list = Customer.query.all()
+    sizes = TableSize.query.all()
+    # create a new event on the first customer in the list
+    event = Event(customer_list[0])
+    # set the default time to 7 days from now at 7 PM
+    time = datetime.datetime.now()
+    time = time.replace(time.year, time.month, time.day+7, hour=19, minute=0)
+    event.time = time
+    # if the method is post, handle that the same way as modify event
+    if request.method == 'POST':
+        # let the function tell us how we should be directed
+        handler = handle_post(event)
+        # handle the redirection
+        return handler
+    # if this is GET, handle that for first time event creation
+    return render_template('add-update-event.html.j2', event=event, sizes=sizes, planner=g.current_user,
+                           customers=customer_list, planners=planner_list,
+                           date=convert_time(event.time), cancel_redirect=url_for('events.list_events'))
 
 
 @events_blueprint.route('/modifyEvent/<event_id>', methods=['GET', 'POST'])
 @multi_auth.login_required
 def modify_event(event_id):
     event = Event.query.get(event_id)
-    if request.method == 'GET':
-        return render_template('add-update-event.html.j2', event=event,
-                               cancel_redirect=url_for('events.list_events'))
-    elif request.method == 'POST' and request.files:
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            home = str(Path.home())
-            path = os.path.join(home, filename)
-            file.save(path)
-            c = g.current_user.company
-            c.process_guest_list(path, event)
-            flash('Import Completed: ' + filename)
+    planner_list = User.query.all()
+    customer_list = Customer.query.all()
+    sizes = TableSize.query.all()
+    # if the method is post, handle that the same way as add event
+    if request.method == 'POST':
+        # let the function tell us how we should be directed
+        handler = handle_post(event)
+        # handle the redirection
+        return handler
+    # if this is GET, handle that for event modification
+    return render_template('add-update-event.html.j2', event=event, sizes=sizes, planner=event.planner,
+                           customers=customer_list, planners=planner_list,
+                           date=convert_time(event.time), cancel_redirect=url_for('events.list_events'))
+
+
+def handle_post(event):
+    planner_list = User.query.all()
+    customer_list = Customer.query.all()
+    sizes = TableSize.query.all()
+    button = request.form.get('button')
+
+    if button == 'save':
+        is_valid = validate_and_save(event, request)
+        if is_valid:
+            flash("{name} added".format(name=event.name), "success")
+            return redirect(url_for('events.list_events'))
         else:
-            flash('File Type Not Accepted', category='error')
+            return render_template('add-update-event.html.j2', event=event, sizes=sizes, planner=event.planner,
+                                   customers=customer_list, planners=planner_list,
+                                   date=convert_time(event.time), cancel_redirect=url_for('events.list_events'))
+    elif button == 'cancel':
+        redirect(url_for('events.list_events'))
+    elif button == 'seat':
+        return redirect(url_for('events.print_seating_chart', id=event.id))
+    elif button == 'attendance':
+        return redirect(url_for('events.attendance_list'))
+    elif button == 'table':
+        return redirect(url_for('events.table_cards'))
+    else:
+        is_valid = validate_and_save(event, request)
+        if is_valid:
+            upload_file(event, request)
+        return render_template('add-update-event.html.j2', event=event, sizes=sizes, planner=event.planner,
+                               customers=customer_list, planners=planner_list,
+                               date=convert_time(event.time), cancel_redirect=url_for('events.list_events'))
 
-    return render_template('add-update-event.html.j2')
 
-
-def upload_file(request, event):
+def upload_file(event, request):
     # check if the post request has the file part
     if 'file' not in request.files:
         flash('No file part')
@@ -109,6 +119,7 @@ def upload_file(request, event):
     else:
         flash('File Type Not Accepted', category='error')
 
+
 @events_blueprint.route('/seatingChart')
 @multi_auth.login_required
 def seating_chart():
@@ -121,14 +132,14 @@ def seating_chart():
 @multi_auth.login_required
 def table_cards():
     # TODO Table Cards
-    return render_template('test.html.j2')
+    return render_template('test.html.j2', test='table_cards')
 
 
 @events_blueprint.route('/attendanceList')
 @multi_auth.login_required
 def attendance_list():
     # TODO Attendance List
-    return render_template('test.html.j2')
+    return render_template('test.html.j2', test='attendance_list')
 
 
 @events_blueprint.route('/printSeatingChartTest')
@@ -144,19 +155,43 @@ def print_seating_chart(id):
 
 
 def validate_and_save(event, request):
-    event.planner = User.query.get(request.form['planner'])
-    event.name = request.form['name']
-    event.time = request.form['time']
-    event.is_done = True if request.form['status'] == "Done" else False
-    event.venue = request.form['venue']
-    event.percent_extra_seats = request.form['extra']
     event.company = g.current_user.company
-    event.table_size = request.form['table_size']
+    event.planner = User.query.filter_by(id=request.form['planner']).one_or_none()
+    event.customer = Customer.query.filter_by(id=request.form['customer']).one_or_none()
+    event.name = request.form['name']
+    event.venue = request.form['venue']
+    # get the html form of the datetime
+    date_in = request.form['time']
+    # convert it into python datetime
+    event.time = datetime.datetime(*[int(v) for v in date_in.replace('T', '-').replace(':', '-').split('-')])
+    event.is_done = True if request.form['status'] == "Done" else False
+    event.table_size = TableSize.query.filter_by(id=request.form['table_size']).one_or_none()
+    event.percent_extra_seats = request.form['extra']
 
     if event.name is None or len(event.name) == 0:
         flash("Name is required", "error")
+        return False
+    elif event.venue is None or len(event.venue) == 0:
+        flash("Venue is required", "error")
+        return False
+    elif event.time is None or event.time < datetime.datetime.now():
+        flash("Valid Date and Time is required", "error")
+        return False
+    elif event.percent_extra_seats is None or len(event.percent_extra_seats) == 0:
+        flash("Extra Seating Percentage is required", "error")
         return False
     else:
         db.session.add(event)
         db.session.commit()
         return True
+
+
+def convert_time(time):
+    time=str(time).replace(" ", "T")
+    date = (time[:16]) if len(time) > 16 else time
+    return date
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in set(['csv'])
+
