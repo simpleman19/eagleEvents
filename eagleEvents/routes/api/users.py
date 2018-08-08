a83f7ac22d62a0164120e4531d7399f8b58dd72d
jacob <jake6642@gmail.com>
2018-08-08 15:33:37 -0500

    users list is working

--- eagleEvents/routes/api/users.py
+++ eagleEvents/routes/api/users.py
@@ -1,5 +1,25 @@
-from flask import Blueprint, request, jsonify, abort
+from flask import Blueprint, request, jsonify, abort, g
 from eagleEvents.models import db
 from eagleEvents.auth import multi_auth
 
 user_api_blueprint = Blueprint('users_api', __name__, url_prefix='/api/user')
+
+
+@user_api_blueprint.route('/users', methods=['GET'])
+@multi_auth.login_required
+def get_all_users():
+    company = g.current_user.company
+    response = {
+        'users': []
+    }
+    if company and len(company.users) > 0:
+        response['users'] = [{
+            'id': u.id,
+            'name': u.name,
+            'username': u.username,
+            'active': u.is_active,
+            'admin': u.is_admin
+        } for u in company.users]
+    else:
+        response = {'error': "No Users Found for " + company.name}
+    return jsonify(response)
