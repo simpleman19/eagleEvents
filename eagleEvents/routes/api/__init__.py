c9c4b750d0ac06739fb9a365e13d9629181c795c
dee-dee-raye <deedee.mccroskey@gmail.com>
2018-08-11 01:35:13 -0500

    almost there with put

--- eagleEvents/routes/api/__init__.py
+++ eagleEvents/routes/api/__init__.py
@@ -1,2 +1,10 @@
+from flask import jsonify
 
+
+def bad_request(message):
+    return jsonify({'error': message}), 400
+
+
+def validation_error(errors):
+    return jsonify({'error': 'Invalid data', 'errors': errors})
 
